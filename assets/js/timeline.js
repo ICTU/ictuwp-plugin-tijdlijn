ef76e57a576ddde9554cd6ec26cb9559ce40ddbf
Paul van Buuren <paul@wbvb.nl>
2026-01-09 10:30:45 +0100

    Still can't figure out how to open subitems without destroying the script

--- assets/js/timeline.js
+++ assets/js/timeline.js
@@ -1,4 +1,3 @@
-
 /*
 // * ICTU / WP timeline. 
 // * 
@@ -12,20 +11,21 @@
  */
 
 
-jQuery.fn.timeline = function() {
-    return this.each(function() {
+jQuery.fn.timeline = function () {
+    return this.each(function () {
+
         function doInitialize() {
-          e.addClass("initialized");
-          setProperties()
+            e.addClass("initialized");
+            setProperties()
         }
 
         function setProperties() {
-            jQuery(".timelineMajor").each(function(e, t) {
+            jQuery(".timelineMajor").each(function (e, t) {
                 jQuery(this).hasClass("open") && jQuery(this).find(".majorEvent").attr({
                     "aria-hidden": !1,
                     "aria-expanded": !0
                 })
-            }), jQuery(".timelineMinor").each(function() {
+            }), jQuery(".timelineMinor").each(function () {
                 jQuery(this).hasClass("open") && jQuery(this).find(".timelineEvent").attr({
                     "aria-hidden": !1,
                     "aria-expanded": !0
@@ -35,73 +35,86 @@ jQuery.fn.timeline = function() {
 
         function doOpenAction() {
 
-            jQuery(".timelineToggle").hasClass("open") ? jQuery(".timelineToggle").find("button").html( string_close ) : jQuery(".timelineToggle").find("button").html( string_open ), e.find(".timelineToggle button").on("click", function(r) {
+            jQuery(".timelineToggle").hasClass("open") ? jQuery(".timelineToggle").find("button").html(string_close) : jQuery(".timelineToggle").find("button").html(string_open), e.find(".timelineToggle button").on("click", function (r) {
                 r.preventDefault();
-                var timelineToggle  = e.find(".timelineToggle"),
-                    timelineMajor   = e.find(".timelineMajor"),
+                var timelineToggle  = e.find(".timelineToggle"),    // buttons
+                    timelineMajor   = e.find(".timelineMajor"),     // hoofditem
                     htmlbody        = jQuery("html,body"),
                     timelinemain    = jQuery(this).parent().parent(".timeline-main");
+
                 navigator.userAgent.match(/(iPod|iPhone|iPad|Android)/) ? window.scrollTo(0, timelinemain.offset().top - htmlbody.offset().top) : htmlbody.animate({
                     scrollTop: timelinemain.offset().top - htmlbody.offset().top,
                     scrollLeft: 0
-                }, 300), timelineToggle.hasClass("open") ? (timelineMajor.each(function() {
-                    doCloseObject(jQuery(this))
-                }), timelineToggle.find("button").html( string_open ), timelineToggle.removeClass("open")) : (timelineMajor.each(function() {
-                    doOpenObject(jQuery(this), !1)
-                }), timelineToggle.find("button").html( string_close ), timelineToggle.addClass("open"))
-            }), e.find(".timelineMajorMarker a").on("click", function(e) {
+                }, 300), timelineToggle.hasClass("open") ? (timelineMajor.each(function () {
+                    // if button has class .open, then close minor objects of major item
+                    doCloseMinorObjectsOfItem(jQuery(this))
+                }),
+                    timelineToggle.find("button").html(string_open),
+                    timelineToggle.removeClass("open")) : (timelineMajor.each(function () {
+                    doOpenMinorObject(jQuery(this), !1)
+                }), timelineToggle.find("button").html(string_close),
+                    timelineToggle.addClass("open"))
+            }),
+            e.find(".timelineMajorMarker a").on("click", function (e) {
                 e.preventDefault();
                 var timelineMajor = jQuery(this).parents(".timelineMajor");
-                timelineMajor.hasClass("open") ? doCloseObject( timelineMajor ) : doOpenObject( timelineMajor, !0)
-            }), e.find(".timelineEventHead a").on("click", function(e) {
+                timelineMajor.hasClass("open") ? doCloseMinorObjectsOfItem(timelineMajor) : doOpenMinorObject(timelineMajor, !0)
+            }),
+            e.find(".timelineEventHead a").on("click", function (e) {
                 e.preventDefault();
                 var timelineMinor = jQuery(this).parents(".timelineMinor");
-                timelineMinor.hasClass("open") ? l( timelineMinor ) : f( timelineMinor )
+                timelineMinor.hasClass("open") ? doRemoveClassOpen(timelineMinor) : doAddClassOpen(timelineMinor)
             })
         }
 
-        function doOpenObject(e, t) {
+        function doOpenMinorObject(e, t) {
             $minors = e.find(".timelineMinor"), e.addClass("open");
+            
             var n = e.find(".majorEvent");
+            
             n.attr({
                 "aria-hidden": !1,
                 "aria-expanded": !0
             }), t && n.focus()
         }
 
-        function doCloseObject(e) {
-            $minors = e.find(".timelineMinor"), $minors.each(function() {
-                l(jQuery(this))
+        function doCloseMinorObjectsOfItem(e) {
+            $minors = e.find(".timelineMinor"), $minors.each(function () {
+                doRemoveClassOpen(jQuery(this))
             });
+
             var t = e.find(".majorEvent");
+
             t.attr({
                 "aria-hidden": !0,
                 "aria-expanded": !1
             }), e.removeClass("open")
         }
 
-        function f(e) {
+        function doAddClassOpen(e) {
             e.addClass("open");
+
             var t = e.find(".timelineEvent");
+
             t.attr({
                 "aria-hidden": !1,
                 "aria-expanded": !0
             }), t.focus()
         }
 
-        function l(e) {
+        function doRemoveClassOpen(e) {
             e.removeClass("open"), e.find(".timelineEvent").attr({
                 "aria-hidden": !0,
                 "aria-expanded": !1
             }), jQuery("video,audio").trigger("pause")
         }
-        var e = jQuery(this),
-            t = jQuery(this).data("expand"),
 
-            string_close  = timeline.toggle_close,
-            string_open   = timeline.toggle_open,
 
-            n = jQuery(this).data("collapse");
+        var e               = jQuery(this),
+            t               = jQuery(this).data("expand"),
+            string_close    = timeline.toggle_close,
+            string_open     = timeline.toggle_open,
+            n               = jQuery(this).data("collapse");
 
         doInitialize()
     })
